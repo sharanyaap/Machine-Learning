0e90d6623d310820593a2297bd07107ae4541c5c
Sharanya <sharanya.p@247-inc.com>
2016-10-25 22:07:34 +0530

    Second commit

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -21,8 +21,7 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-ur = U(:,1:K);
-X_rec = Z * ur';
+
 
 % =============================================================
 
